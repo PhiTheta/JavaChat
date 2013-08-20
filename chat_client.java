a3bbe06a9d02a51e3fe5b92805e32d2ad98e44ff
harryi3t <2011195@iiitdmj.ac.in>
2013-08-20 10:08:50 +0530

    test commit

--- chat_client.java
+++ chat_client.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class chat_client {
-	/***************************************************************************
+	/*****************************************************************************
 	 * Author: Harendra Singh
 	 * Program Flow: 
 	 * 		waitForServer ---> startThreads (incoming,outgoing)
